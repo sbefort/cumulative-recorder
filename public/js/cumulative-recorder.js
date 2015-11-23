b1c985d39a620d1c605ed33a03f841561880ebb9
nomad <sbefort@gmail.com>
2015-11-22 22:44:01 -0600

    Added a basic demo of a live streaming CanvasJS chart to the index page.

---
+++ public/js/cumulative-recorder.js
@@ -0,0 +1,50 @@
+
+window.onload = function () {
+
+	var dps = []; // dataPoints
+
+	var chart = new CanvasJS.Chart("cumulative-recorder",{
+		title :{
+			text: "Cumulative Recorder Demo",
+			fontFamily: "Helvetica Neue,Helvetica,Arial,sans-serif",
+			fontSize: 30,
+		},			
+		data: [{
+			type: "line",
+			dataPoints: dps 
+		}]
+	});
+
+	var xVal = 0;
+	var yVal = 100;	
+	var updateInterval = 100;
+	var dataLength = 500; // number of dataPoints visible at any point
+
+	var updateChart = function (count) {
+		count = count || 1;
+		// count is number of times loop runs to generate random dataPoints.
+		
+		for (var j = 0; j < count; j++) {	
+				yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+				dps.push({
+					x: xVal,
+					y: yVal
+				});
+				xVal++;
+			};
+			if (dps.length > dataLength)
+		{
+			dps.shift();				
+		}
+		
+		chart.render();		
+
+	};
+
+	// generates first set of dataPoints
+	updateChart(dataLength); 
+
+	// update chart after specified time. 
+	setInterval(function(){updateChart()}, updateInterval); 
+
+}
